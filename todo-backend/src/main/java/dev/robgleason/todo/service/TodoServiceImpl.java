ffaec18ed15ca3c27a42c7b98678f03eb3c5ef59
Robert Gleason <l2ob122@gmail.com>
2023-09-05 20:42:32 -0700

    added model mapper to make it easier to converting dtos

--- todo-backend/src/main/java/dev/robgleason/todo/service/TodoServiceImpl.java
+++ todo-backend/src/main/java/dev/robgleason/todo/service/TodoServiceImpl.java
@@ -5,6 +5,7 @@ import dev.robgleason.todo.dto.TodoDto;
 import dev.robgleason.todo.entity.Todo;
 import dev.robgleason.todo.repository.TodoRepository;
 import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,26 +13,18 @@ import org.springframework.stereotype.Service;
 public class TodoServiceImpl implements TodoService {
 
     private TodoRepository todoRepository;
+    private ModelMapper modelMapper;
 
     @Override
     public TodoDto addTodo(TodoDto todoDto) {
-        // convert TodoDto to Jpa Entity
 
-        Todo todo = new Todo();
-        todo.setTitle(todoDto.getTitle());
-        todo.setDescription(todoDto.getDescription());
-        todo.setCompleted(todoDto.getCompleted());
+        Todo todo = modelMapper.map(todoDto, Todo.class);
 
-        // Todo Jpa entity
         Todo savedTodo = todoRepository.save(todo);
 
-        // convert saved jpa entity object into TodoDto
-        TodoDto savedDto = new TodoDto();
-        savedDto.setId(savedTodo.getId());
-        savedDto.setTitle(savedTodo.getTitle());
-        savedDto.setDescription(savedTodo.getDescription());
-        savedDto.setCompleted(savedTodo.getCompleted());
+        TodoDto savedTodoDto = modelMapper.map(savedTodo, TodoDto.class);
 
-        return savedDto;
+
+        return savedTodoDto;
     }
 }
