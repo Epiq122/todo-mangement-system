9206f1180e610ea3ce831160915f0a5dd1f8ff7e
Robert Gleason <l2ob122@gmail.com>
2023-09-06 21:05:22 -0700

    added footer and header

--- todo-frontend/src/App.jsx
+++ todo-frontend/src/App.jsx
@@ -2,13 +2,17 @@ import { useState } from "react";
 
 import "./App.css";
 import ListTodoComponent from "./components/ListTodoComponent.jsx";
+import HeaderComponent from "./components/HeaderComponent.jsx";
+import FooterComponent from "./components/FooterComponent.jsx";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div>
+      <HeaderComponent />
       <ListTodoComponent />
+      <FooterComponent />
     </div>
   );
 }
---
+++ todo-frontend/src/components/FooterComponent.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const FooterComponent = () => {
+  return (
+    <div>
+      <footer className="footer">
+        <p className="mt-3">All rights reserved by Rob Gleason</p>
+      </footer>
+    </div>
+  );
+};
+export default FooterComponent;
