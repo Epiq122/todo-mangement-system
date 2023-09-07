b9f0515edd11de2ec2d94d155226e5315a068987
Robert Gleason <l2ob122@gmail.com>
2023-09-06 21:18:54 -0700

    installed react router dom, added a route for the main page

--- todo-frontend/src/App.jsx
+++ todo-frontend/src/App.jsx
@@ -4,16 +4,25 @@ import "./App.css";
 import ListTodoComponent from "./components/ListTodoComponent.jsx";
 import HeaderComponent from "./components/HeaderComponent.jsx";
 import FooterComponent from "./components/FooterComponent.jsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div>
-      <HeaderComponent />
-      <ListTodoComponent />
-      <FooterComponent />
-    </div>
+    <>
+      <BrowserRouter>
+        <HeaderComponent />
+        <Routes>
+          {/*http://localhost:8080*/}
+          <Route path="/" element={<ListTodoComponent />} />
+          {/*http://localhost:8080/todos*/}
+          <Route path="/todos" element={<ListTodoComponent />} />
+        </Routes>
+
+        <FooterComponent />
+      </BrowserRouter>
+    </>
   );
 }
 
