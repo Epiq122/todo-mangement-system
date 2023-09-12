6643f8196a3eddb477b17e2b9b6f9a4f3ac99ac0
Robert Gleason <l2ob122@gmail.com>
2023-09-11 19:05:44 -0700

    created basic auth secueity, created config file

---
+++ todo-backend/src/main/java/dev/robgleason/todo/config/SpringSecurityConfig.java
@@ -0,0 +1,26 @@
+package dev.robgleason.todo.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SpringSecurityConfig {
+
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf((csrf) -> csrf.disable())
+                .authorizeHttpRequests((authorize) ->{
+                    authorize.anyRequest().authenticated();
+                }).httpBasic(Customizer.withDefaults());
+        return http.build();
+
+    }
+}
+
+
+//http.csrf((csrf) -> csrf.disable())
