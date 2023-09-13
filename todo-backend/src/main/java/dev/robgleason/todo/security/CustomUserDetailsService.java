be491b4f838df33487a214ce7618e0ef54e3cc04
Robert Gleason <l2ob122@gmail.com>
2023-09-12 19:46:34 -0700

    created custom user details

---
+++ todo-backend/src/main/java/dev/robgleason/todo/security/CustomUserDetailsService.java
@@ -0,0 +1,38 @@
+package dev.robgleason.todo.security;
+
+import dev.robgleason.todo.entity.User;
+import dev.robgleason.todo.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+
+@Service
+@AllArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String usernameOrEmail) throws UsernameNotFoundException {
+        User user = userRepository.findByUsernameOrEmail(usernameOrEmail, usernameOrEmail)
+                .orElseThrow(() -> new UsernameNotFoundException("Username or Email does not exist"));
+
+        Set<GrantedAuthority> authorities = user.getRoles().stream()
+                .map((role) -> new SimpleGrantedAuthority(role.getName()))
+                .collect(Collectors.toSet());
+
+
+        return new org.springframework.security.core.userdetails.User(usernameOrEmail,
+                user.getPassword(),
+                authorities
+        );
+    }
+}
